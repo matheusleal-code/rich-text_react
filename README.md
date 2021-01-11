d650684732daa54047b973dfd847e171df1d1e76
matheusleal-codem <alanwalkker.123@gmail.com>
2021-01-11 20:50:41 -0300

    finished project

---
+++ README.md
@@ -0,0 +1,58 @@
+# Rich-Text
+
+A small example of a rich-text application with only 3 methods, italic, bold and underlined
+
+## About the Project
+
+![Screenshot](view.png)
+
+## Built With
+
+React
+Css
+Html
+
+## Prerequisites
+
+```
+* npm
+npm install npm@latest -g
+
+```
+
+* [React](https://reactjs.org/)
+
+```
+
+*[NodeJS](https://nodejs.org/en/download/)
+
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Clone the repo
+
+```
+https://github.com/matheusleal-code/rich-text_react.git
+```
+
+install NPM packages
+
+```
+npm install
+```
+
+## Built With
+
+* [React](https://reactjs.org/) - The web framework used
+* [Nodejs](https://nodejs.org/en/download/) - The web backend javascript 
+
+## Authors
+
+* **Matheus Leal**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/main/RichText.jsx
+++ src/main/RichText.jsx
@@ -5,9 +5,12 @@ import Button from '../components/Button'
 import Input from '../components/Input'
 
 const initialState = {
-    text: 'texto',
+    text: '',
     style:'',
-    styles : []
+    styles : [],
+    boldClick: true,
+    italicClick: true,
+    underlineClick: true
 }
 class RichText extends Component{
 
@@ -17,8 +20,19 @@ class RichText extends Component{
         this.buttonActionItalic = this.buttonActionItalic.bind(this)
         this.buttonActionUnderline = this.buttonActionUnderline.bind(this)
         this.buttonActionBold = this.buttonActionBold.bind(this)
+        this.styleAtt = this.styleAtt.bind(this)
         this.state = initialState
         this.handleChange = this.handleChange.bind(this)
+    }
+
+    styleAtt(){
+        let styles = ''
+        for(let i = 0; i < this.state.styles.length; i++){
+            styles += ' '+this.state.styles[i]
+        }
+        this.state.style = styles
+        console.log(this.state)
+
     }
 
     handleChange(e){
@@ -27,15 +41,52 @@ class RichText extends Component{
     }
 
     buttonActionItalic(){
-        this.setState({style: 'italic'})
+        if(this.state.italicClick){
+            this.state.styles.push('italic')
+            this.setState({styles: this.state.styles})
+            this.setState({italicClick: false})
+            this.styleAtt()
+     
+        }else{
+            const elemento = this.state.styles.indexOf('italic')
+            this.state.styles.splice(elemento)
+            this.setState({italicClick: true})
+            this.styleAtt()
+        }
     }
 
     buttonActionBold(){
-        this.setState({style: 'bold'})
+        if(this.state.boldClick){
+            this.state.styles.push('bold')
+            this.setState({styles: this.state.styles})
+            this.setState({boldClick: false})
+            this.styleAtt()
+      
+        }else{
+            const elemento = this.state.styles.indexOf('bold')
+            this.state.styles.splice(elemento)
+            this.setState({boldClick: true})
+            this.styleAtt()
+        }
+
+
     }
 
     buttonActionUnderline(){
-        this.setState({style: 'underline'})
+        if(this.state.underlineClick){
+            this.setState({underlineClick: false})
+            this.state.styles.push('underline')
+            this.setState({styles: this.state.styles})
+            this.styleAtt()
+  
+        }else{
+            const elemento = this.state.styles.indexOf('underline')
+            this.state.styles.splice(elemento)
+            this.setState({underlineClick: true})
+            this.styleAtt()
+        }
+
+ 
     }
     
     render(){
